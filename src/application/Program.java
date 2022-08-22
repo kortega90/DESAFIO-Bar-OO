bb6e3d4e3361e1e49fdd8e1a1cc2207dd7560f9f
KENGI ORTEGA <kortega90@hotmail.com>
2022-08-22 17:30:26 -0300

    first commit

---
+++ src/application/Program.java
@@ -0,0 +1,34 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Bill;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		Bill bill = new Bill();
+		
+		System.out.println("Sexo: ");
+		char gender_= sc.next().charAt(0);
+		bill.gender = Character.toUpperCase(gender_);
+		System.out.println("Quantidade de cervejas: ");
+		bill.beer = sc.nextInt();
+		System.out.println("Quantidade de refrigerantes: ");
+		bill.softDrink = sc.nextInt();
+		System.out.println("Quantidade de espetinhos: ");
+		bill.barbecue = sc.nextInt();
+		
+		System.out.println("RELATÓRIO:");
+		
+	
+	}
+
+}
---
+++ src/entities/Bill.java
@@ -0,0 +1,13 @@
+package entities;
+
+public class Bill {
+
+public char gender;
+public int beer;
+public int barbecue;
+public int softDrink;
+
+public double cover() {
+	 return 
+}
+}
